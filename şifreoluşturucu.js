07c49574fd588ba083b01245ce59f64c00e8d9ee
Dola123312 <manyakoyuncu999@gmail.com>
2020-07-28 18:10:19 +0300

    Update şifreoluşturucu.js

--- şifreoluşturucu.js
+++ şifreoluşturucu.js
@@ -1,5 +1,4 @@
-var değerler = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-var sonuç = "";
+var değerler = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",sonuç = "";
 function şifreoluşturucu() {
     for (var i = 0, u = değerler.length; i < 16; ++i) {
         sonuç += değerler.charAt(Math.floor(Math.random() * u));
