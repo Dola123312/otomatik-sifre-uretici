9e32dbb3e7957c0a171b83e684b80b330fff9425
Hakan Engin <manyakoyuncu999@gmail.com>
2020-08-01 18:22:52 +0300

    Update şifreoluşturucu.js

--- şifreoluşturucu.js
+++ şifreoluşturucu.js
@@ -1,4 +1,4 @@
-var degerler = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",sonuc = "";
+var degerler = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789",sonuc = "";
 function sifreolusturucu() {
     for (var i = 0, u = degerler.length; i < 16; ++i) {
         sonuc += degerler[Math.floor(Math.random() * u)];
